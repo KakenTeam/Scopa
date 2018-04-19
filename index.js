4edc36c2598801b215e395e0dfa4115541ade54b
thuan274 <doanminhthuan274@gmail.com>
2018-04-19 22:20:45 +0700

    default set free

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ io.on('connection', function (socket) {
 
   socket.on('connection', function (message) {
     console.log(message);
+    set_state_arduino(false);
   });
 
   socket.on("done", (order_id) => {
