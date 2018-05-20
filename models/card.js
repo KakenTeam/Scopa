1671cef3a9656a28f13bdc4b062d2138ebe72b0b
minhthuan <doanminhthuan274@gmail.com>
2018-05-21 05:41:53 +0700

    new generate id card

--- models/card.js
+++ models/card.js
@@ -1,12 +1,20 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-var Card = mongoose.model('Card', {
+var CardSchema = new mongoose.Schema({
   amount: {
     type: Number,
     default: 4000
   }
-})
+});
 
+CardSchema.pre('save', function (next) {
+  var card = this;
+  var id_card = Math.floor(Math.random() * 999999) + 100000;
+  card._id = id_card;
+  next();
+});
+
+
+var Card = mongoose.model('Card', CardSchema);
 
 module.exports = { Card }
